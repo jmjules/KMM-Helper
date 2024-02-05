41602935fdc7118170387aff4eac8fa878028be5
jmjules <muschinskijulian@gmail.com>
2024-02-05 22:59:41 +0100

    added key when iteration over classes

--- src/app/stundenplan/components/TimetableDisplay.tsx
+++ src/app/stundenplan/components/TimetableDisplay.tsx
@@ -28,21 +28,22 @@ export default function TimetableDisplay( {selectedDay, data} ) {
 
   return (
     <>
-    
-    {data[selectedDay].map( (entry : ClassEntry) => (
-                <div className="bg-purple-800 rounded-lg m-2 p-1 flex gap-2">
-                    <p className="text-sm text-center leading-none self-center"> {entry.start} <br /> I <br /> {entry.end}</p>
-                    <div>
-                      <p className="text-2xl  font-bold"> {entry.name} </p>
-                      <p className="text-sm"> {entry.room} </p>
-                      <p className="text-sm"> {entry.prof} </p>
-                      <p className="text-sm"> {entry.body} </p>
-                      <a href={entry.link[0]} target="_blank" className="text-green-500" > {entry.link[1]} </a>
-                    </div>
-                </div>
-            ))}
-    
-    
+      {Object.keys(data[selectedDay]).map((classKey) => (
+        <div key={classKey} className="bg-purple-800 rounded-lg m-2 p-1 flex gap-2">
+          <p className="text-sm text-center leading-none self-center">
+            {data[selectedDay][classKey].start} <br /> I <br /> {data[selectedDay][classKey].end}
+          </p>
+          <div>
+            <p className="text-2xl font-bold">{data[selectedDay][classKey].name}</p>
+            <p className="text-sm">{data[selectedDay][classKey].room}</p>
+            <p className="text-sm">{data[selectedDay][classKey].prof}</p>
+            <p className="text-sm">{data[selectedDay][classKey].body}</p>
+            <a href={data[selectedDay][classKey].link[0]} target="_blank" className="text-green-500">
+              {data[selectedDay][classKey].link[1]}
+            </a>
+          </div>
+        </div>
+      ))}
     </>
   )
 }
