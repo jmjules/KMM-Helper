ea62971d5f7081b9bf8d22ec771467fac18480bb
jmjules <muschinskijulian@gmail.com>
2024-02-06 00:09:31 +0100

    checking if localstorage exists

--- src/app/components/HowToNotice.jsx
+++ src/app/components/HowToNotice.jsx
@@ -4,7 +4,11 @@ import { useState, useEffect } from "react";
 
 export default function HowToNotice() {
 
-    let initialHiddenState = localStorage.getItem("noticeAcknowledged") === "true";
+    // Check if localStorage is available
+    const isLocalStorageAvailable = typeof localStorage !== 'undefined';
+
+    // Retrieve the initial hidden state from localStorage or default to false
+    const initialHiddenState = isLocalStorageAvailable && localStorage.getItem("noticeAcknowledged") === "true";
 
     const [isHidden, setIsHidden] = useState(initialHiddenState);
 
@@ -30,7 +34,8 @@ export default function HowToNotice() {
         <button 
          className='bg-green-700 px-4 py-2 rounded-lg'
          onClick={savetNoticeAcknowledged}
-         >Okay</button>
+         >Okay
+        </button>
     </>
   )
 }
