e0dd8ab793ba13097176dcd998649ddebda9e158
jmjules <muschinskijulian@gmail.com>
2024-04-14 23:39:25 +0200

    adjusted colors to fit theme

--- src/app/semesterdaten/page.tsx
+++ src/app/semesterdaten/page.tsx
@@ -9,27 +9,29 @@ export default async function SemesterData () {
   const semester = data.rahmendaten;
 
   return (
-    <main className="min-h-screen flex-col items-center justify-between">
+    <>
       <SubPageHeader pageTitle="Semesterdaten" />
       
 
 
-      <div className="p-6 bg-orange-700 m-2 rounded-lg">
-        <h2 className="text-2xl font-bold"> {data.title} </h2>
-        <p className="text-xs"> {semester.start} - {semester.end} </p>
-        <h2 className="text-xl mt-3">Vorlesungszeit</h2>
-        <p className="text-xs"> {semester.startOfClasses} - {semester.endOfClasses} </p>
+      <div className="max-w-[690px] mx-auto">
+        <div className="m-5 p-6 rounded-lg bg-neutral-800">
+          <h2 className="text-2xl font-bold"> {data.title} </h2>
+          <p className="text-xs"> {semester.start} - {semester.end} </p>
+          <h2 className="text-xl mt-3">Vorlesungszeit</h2>
+          <p className="text-xs"> {semester.startOfClasses} - {semester.endOfClasses} </p>
         
-        <h2 className="text-xl mt-2">Prüfungszeit</h2>
-        <p className="text-xs"> {semester.startOfExams} - {semester.endOfExams} </p>
+          <h2 className="text-xl mt-2">Prüfungszeit</h2>
+          <p className="text-xs"> {semester.startOfExams} - {semester.endOfExams} </p>
         
-        <h2 className="text-xl mt-2">Vorlesungsfreie Tage</h2>
-        <p className="text-xs"> {semester.vacationDays[0].date} ({semester.vacationDays[0].reason}) </p>
-        <p className="text-xs"> {semester.vacationDays[1].date} - {semester.vacationDays[4].date} ({semester.vacationDays[1].reason}) </p>
+          <h2 className="text-xl mt-2">Vorlesungsfreie Tage</h2>
+          <p className="text-xs"> {semester.vacationDays[0].date} ({semester.vacationDays[0].reason}) </p>
+          <p className="text-xs"> {semester.vacationDays[1].date} - {semester.vacationDays[4].date} ({semester.vacationDays[1].reason}) </p>
+        </div>
       </div>
     
     
     
-    </main>
+    </>
   )
 }
